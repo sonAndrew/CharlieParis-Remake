a1249ba81db3f472e233907f9be0d35ad3341967
Andrew <andrewz.jackson667@gmail.com>
2018-08-10 00:13:08 -0400

    created collection head title and discover button

--- src/app/Components/collection-head/collection-head.component.ts
+++ src/app/Components/collection-head/collection-head.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-collection-head',
   templateUrl: './collection-head.component.html',
-  styleUrls: ['./collection-head.component.css']
+  styleUrls: ['./collection-head.component.scss']
 })
 export class CollectionHeadComponent implements OnInit {
 
