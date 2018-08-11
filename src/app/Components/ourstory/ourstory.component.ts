a5e9ae8fb24ceae6f6fa12ac6e5d21df2d2545f5
Andrew <andrewz.jackson667@gmail.com>
2018-08-11 16:43:13 -0400

    spaced out slider item text from ourstory componet.

--- src/app/Components/ourstory/ourstory.component.ts
+++ src/app/Components/ourstory/ourstory.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-ourstory',
   templateUrl: './ourstory.component.html',
-  styleUrls: ['./ourstory.component.css']
+  styleUrls: ['./ourstory.component.scss']
 })
 export class OurstoryComponent implements OnInit {
 
