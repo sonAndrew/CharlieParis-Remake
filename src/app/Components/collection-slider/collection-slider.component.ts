b6fc3ca2b621ea7b6487d08088559284f54c3659
Andrew <andrewz.jackson667@gmail.com>
2018-08-11 16:13:02 -0400

    fixed navbar icons and dropdown content for mobile

--- src/app/Components/collection-slider/collection-slider.component.ts
+++ src/app/Components/collection-slider/collection-slider.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-collection-slider',
@@ -9,23 +8,15 @@ import {map} from 'rxjs/operators';
 export class CollectionSliderComponent implements OnInit {
 
   collection:any = ['NOVELTIES', 'MEN', 'WOMAN'];
-  images: Array<string>;
-  // [
-  //   '../../../assets/Images/art-blur-brass.jpg',
-  //   '../../../assets/Images/analogue-color-executive.jpg',
-  //   '../../../assets/Images/analogue-antique-business.jpg'
-  // ]
+  images:any = [
+    '../../../assets/Images/art-blur-brass.jpg',
+    '../../../assets/Images/analogue-color-executive.jpg',
+    '../../../assets/Images/analogue-antique-business.jpg'
+  ]
 
   constructor() {}
 
   ngOnInit() {
-  }
-
-  private _randomImageUrls(images: Array<{id: number}>): Array<string> {
-    return [1, 2, 3].map(() => {
-      const randomId = images[Math.floor(Math.random() * images.length)];
-      return `../../../assets/{randomId}`;
-    });
   }
 
 }
