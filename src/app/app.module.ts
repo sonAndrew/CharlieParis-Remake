a5ca9ca403d10c407fdca8cf06f9601fe22f1781
Andrew <andrewz.jackson667@gmail.com>
2018-08-11 16:39:40 -0400

    Changed the width on slider and added text about each item in slider. Created our story component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { NavbarComponent } from './Components/navbar/navbar.component';
 import { HeroImgComponent } from './Components/hero-img/hero-img.component';
 import { CollectionHeadComponent } from './Components/collection-head/collection-head.component';
 import { CollectionSliderComponent } from './Components/collection-slider/collection-slider.component';
+import { OurstoryComponent } from './Components/ourstory/ourstory.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { CollectionSliderComponent } from './Components/collection-slider/collec
     NavbarComponent,
     HeroImgComponent,
     CollectionHeadComponent,
-    CollectionSliderComponent
+    CollectionSliderComponent,
+    OurstoryComponent
   ],
   imports: [
     BrowserModule,
