d6bfe7820a06220246ae2011a33772487f612c26
Andrew <andrewz.jackson667@gmail.com>
2018-08-12 12:01:58 -0400

    Tried to work on angular animation of images in an animate.ts. It took too much time to try and do so I will come back to it at a later date

--- src/app/Components/ourstory/ourstory.component.ts
+++ src/app/Components/ourstory/ourstory.component.ts
@@ -1,13 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+//import { fade } from './animate.ts';
+
 
 @Component({
   selector: 'app-ourstory',
-  templateUrl: './ourstory.component.html',
-  styleUrls: ['./ourstory.component.scss']
+  templateUrl: 'ourstory.component.html',
+  styleUrls: ['./ourstory.component.scss'],
+  //animations: fade
 })
 export class OurstoryComponent implements OnInit {
+  // ourImages:any = [
+  //   '../../../assets/Images/art-blur-brass.jpg'
+  //   '../../../assets/Images/analogue-color-executive.jpg',
+  //   '../../../assets/Images/analogue-antique-business.jpg'
+  // ]
 
   constructor() { }
+
+  // constructor(public name: string, public state = 'inactive') { }
+  // toggleState() {
+  //   this.state = this.state === 'active' ? 'inactive' : 'active';
+  // }
+  
 
   ngOnInit() {
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './Components/navbar/navbar.component';
@@ -20,7 +21,8 @@ import { OurstoryComponent } from './Components/ourstory/ourstory.component';
   ],
   imports: [
     BrowserModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
