1abe47fc4e3b191c011ca09adb4c06389e7a3dba
Andrew <andrewz.jackson667@gmail.com>
2018-08-13 19:01:38 -0400

    ourstory content section designed w/out animation for mobile

--- src/app/Components/ourstory/animate.ts
+++ src/app/Components/ourstory/animate.ts
@@ -1,26 +1,26 @@
 //import { trigger, state, style, animate, transition } from '@angular/animations';
 
 //export const fade = [
-    trigger('imageState', [
-        state('void', style({ 
-            content: 'url(../../../assets/Images/analogue-antique-business.jpg)',
-            width: '100%',
-            height: '250px',
-            padding: '18px',
-            opacity: 0 
-        })),
-        transition( 'void => *', [
-            animate(10000, style({
-                content: 'url(../../../assets/Images/analogue-color-executive.jpg)',
-                opacity: 2
-            }));
-        ]),
-        // transition( '* => *', [
-        //     animate(5000, style({
-        //         content: 'url../../../assets/Images/art-blur-brass.jpg)',
-        //         opacity: 1
-        //     }));
-        // ])
+//     trigger('imageState', [
+//         state('void', style({ 
+//             content: 'url(../../../assets/Images/analogue-antique-business.jpg)',
+//             width: '100%',
+//             height: '250px',
+//             padding: '18px',
+//             opacity: 0 
+//         })),
+//         transition( 'void => *', [
+//             animate(10000, style({
+//                 content: 'url(../../../assets/Images/analogue-color-executive.jpg)',
+//                 opacity: 2
+//             }));
+//         ]),
+//         // transition( '* => *', [
+//         //     animate(5000, style({
+//         //         content: 'url../../../assets/Images/art-blur-brass.jpg)',
+//         //         opacity: 1
+//         //     }));
+//         // ])
   
-      ])
-]
+//       ])
+// ]
