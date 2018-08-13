0bea733ab42f8b03c3842c88dce26e91e6cfdac2
Andrew <andrewz.jackson667@gmail.com>
2018-08-13 19:24:25 -0400

    created html for endcontent section and added image

--- src/app/Components/end-content/end-content.component.ts
+++ src/app/Components/end-content/end-content.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-end-content',
   templateUrl: './end-content.component.html',
-  styleUrls: ['./end-content.component.css']
+  styleUrls: ['./end-content.component.scss']
 })
 export class EndContentComponent implements OnInit {
 
