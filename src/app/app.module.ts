7e932d1328b978b8ed6dde6b654979dac8bd5c0d
Andrew <andrewz.jackson667@gmail.com>
2018-08-15 11:44:11 -0400

    combined hero image and collection head components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './Components/navbar/navbar.component';
-import { HeroImgComponent } from './Components/hero-img/hero-img.component';
 import { CollectionHeadComponent } from './Components/collection-head/collection-head.component';
 import { CollectionSliderComponent } from './Components/collection-slider/collection-slider.component';
 import { OurstoryComponent } from './Components/ourstory/ourstory.component';
@@ -15,7 +14,6 @@ import { EndContentComponent } from './Components/end-content/end-content.compon
   declarations: [
     AppComponent,
     NavbarComponent,
-    HeroImgComponent,
     CollectionHeadComponent,
     CollectionSliderComponent,
     OurstoryComponent,
